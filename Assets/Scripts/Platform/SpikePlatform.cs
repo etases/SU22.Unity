a8b9317e98b47d06c0f9bd09212403cf6eba988e
HSGamer <huynhqtienvtag@gmail.com>
2022-06-29 17:28:18 +0700

    make Platform abstract

--- Assets/Scripts/Platform/SpikePlatform.cs
+++ Assets/Scripts/Platform/SpikePlatform.cs
@@ -1,18 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpikePlatform : MonoBehaviour
+public class SpikePlatform : Platform
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
