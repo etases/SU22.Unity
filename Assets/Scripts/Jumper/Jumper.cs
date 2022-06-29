76c09e55666d9f518657525e84ea9145622a1c02
HSGamer <huynhqtienvtag@gmail.com>
2022-06-29 16:30:48 +0700

    Apply better jumper logic

--- Assets/Scripts/Jumper/Jumper.cs
+++ Assets/Scripts/Jumper/Jumper.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Jumper : MonoBehaviour
@@ -13,10 +11,10 @@ public class Jumper : MonoBehaviour
 
 
     [Header("Status")]
-    private float moveInput;
-    public float jumpValue = 0.0f;
+    public float moveInput;
+    public float jumpValue;
+    public float jumpDirection;
     public bool isGrounded;// check if game object is touching the platform
-    public float posY;
 
     private Rigidbody2D rb;
     private SpriteRenderer spRender;
@@ -32,7 +30,11 @@ public class Jumper : MonoBehaviour
 
     void Update()
     {
+        // Check Is Grounded
+        isGrounded = rb.velocity.y == 0;
+
         moveInput = Input.GetAxisRaw("Horizontal");
+        var isJump = Input.GetAxisRaw("Jump") != 0;
 
         //set animation
         setAnim();
@@ -43,118 +45,46 @@ public class Jumper : MonoBehaviour
         else if (rb.velocity.x > 0)
             spRender.flipX = false;
 
-        //walk
-        if (jumpValue == 0.0f && isGrounded)
+        if (!isGrounded) return;
+        if (isJump) // Charge
         {
-            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
-        }
+            jumpDirection = moveInput switch
+            {
+                > 0 => walkSpeed,
+                < 0 => -walkSpeed,
+                _ => 0
+            };
 
-
-
-        //charge jump 
-        if ((Input.GetKey("space") || (Input.GetKey("e")) || (Input.GetKey("q"))) && isGrounded)
-        {
             if (jumpValue <= 10f)
             {
                 jumpValue += 0.1f;
             }
         }
-
-        //jump up
-        if (Input.GetKeyUp("space"))
+        else
         {
-            if (isGrounded)
+            if (jumpValue != 0) // Jump
             {
                 Jump();
             }
-
-        }
-        //jump right
-        else if (Input.GetKeyUp("e"))
-        {
-            if (isGrounded)
+            else // Walk
             {
-                JumpRight();
+                rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
             }
-
         }
-        //jump left
-        else if (Input.GetKeyUp("q"))
-        {
-            if (isGrounded)
-            {
-                JumpLeft();
-            }
-
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        isGrounded = true;
-        jumpValue = 0f;
-        print("enter");
-    }
-    void OnCollisionExit2D(Collision2D col)
-    {
-        isGrounded = false;
-        print("exit");
-    }
-
-    void resetJump()
-    {
-        jumpValue = 0.0f;
     }
 
     void Jump()
     {
-        rb.velocity = new Vector2(0f, jumpValue * jumpSpeed);
-    }
-    void JumpLeft()
-    {
-        rb.velocity = new Vector2(-walkSpeed, jumpValue * jumpSpeed);
-    }
-    void JumpRight()
-    {
-        rb.velocity = new Vector2(walkSpeed, jumpValue * jumpSpeed);
+        rb.velocity = new Vector2(jumpDirection, jumpValue * jumpSpeed);
+        jumpValue = 0.0f;
     }
 
     void setAnim()
     {
-        //run
-        if (moveInput != 0 && isGrounded == true)
-        {
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
-        }
-
-        //charge
-        if (jumpValue > 0 && isGrounded == true)
-        {
-            anim.SetBool("isCharge", true);
-        }
-        else if (jumpValue > 0 && isGrounded == false)
-        {
-            anim.SetBool("isCharge", false);
-        }
-
-        //jump
-        if(rb.velocity.y>0)
-        {
-            anim.SetBool("isJumping", true);
-        }
-        else if(rb.velocity.y < 0)
-        {
-            anim.SetBool("isJumping", false);
-            anim.SetBool("isFalling", true);
-        }
-        else
-        {
-            anim.SetBool("isFalling", false);
-        }
+        anim.SetBool("isRunning", jumpValue == 0 && isGrounded);
+        anim.SetBool("isCharge", jumpValue > 0 && isGrounded);
+        anim.SetBool("isJumping", !isGrounded && rb.velocity.y > 0);
+        anim.SetBool("isFalling", !isGrounded && rb.velocity.y < 0);
     }
 
 }
