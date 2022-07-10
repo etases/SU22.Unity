1b4fcff619fc601dfff8f454488de333042a755f
HSGamer <huynhqtienvtag@gmail.com>
2022-07-10 11:27:08 +0700

    Unified EventManager

--- Assets/Scripts/Jumper/Jumper.cs
+++ Assets/Scripts/Jumper/Jumper.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Jumper : MonoBehaviour
@@ -7,44 +8,44 @@ public class Jumper : MonoBehaviour
     public const float dJumpCharge = 0.05f;
     public const float dJumpChargeMax = 10f;
 
-    [Header("Speed Settings")]
-    public float walkSpeed = dWalkSpeed;
+    [Header("Speed Settings")] public float walkSpeed = dWalkSpeed;
+
     public float jumpSpeed = dJumpSpeed;
     public float jumpCharge = dJumpCharge;
     public float jumpChargeMax = dJumpChargeMax;
 
+    [Header("Status")] public float moveInput;
 
-    [Header("Status")]
-    public float moveInput;
     public float jumpValue;
     public float jumpDirection;
-    public bool isGrounded;// check if game object is touching the platform
+    public bool isGrounded; // check if game object is touching the platform
+    private Animator anim;
 
     private Rigidbody2D rb;
     private SpriteRenderer spRender;
-    private Animator anim;
 
-    void Start()
+    private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         spRender = gameObject.GetComponent<SpriteRenderer>();
         anim = gameObject.GetComponent<Animator>();
-       
     }
 
-    void Update()
+    private void Update()
     {
         // Check Is Grounded
         if (isGrounded)
         {
-            if (rb.velocity.y < -0.05)
-            {
-                isGrounded = false;
-            }
+            if (rb.velocity.y < -0.05) isGrounded = false;
         }
         else
         {
             isGrounded = rb.velocity.y == 0;
+            if (isGrounded)
+                SimpleEventManager.TriggerEvent("GroundEvent", new Dictionary<string, object>
+                {
+                    {"jumper", this}
+                });
         }
 
         if (!isGrounded)
@@ -57,7 +58,7 @@ public class Jumper : MonoBehaviour
         var isJump = Input.GetAxisRaw("Jump") != 0;
 
         // set animation
-        setAnim();
+        SetAnim();
 
         // flip
         spRender.flipX = rb.velocity.x switch
@@ -77,10 +78,7 @@ public class Jumper : MonoBehaviour
                 _ => 0
             };
 
-            if (jumpValue <= jumpChargeMax)
-            {
-                jumpValue += jumpCharge;
-            }
+            if (jumpValue <= jumpChargeMax) jumpValue += jumpCharge;
         }
         else
         {
@@ -91,25 +89,41 @@ public class Jumper : MonoBehaviour
             else // Walk
             {
                 rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+                SimpleEventManager.TriggerEvent("WalkEvent", new Dictionary<string, object>
+                {
+                    {"jumper", this}
+                });
             }
         }
     }
 
-    void Jump()
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+            SimpleEventManager.TriggerEvent("HitWallEvent", new Dictionary<string, object>
+            {
+                {"jumper", this},
+                {"wall", col.gameObject}
+            });
+    }
+
+    private void Jump()
     {
         rb.velocity = new Vector2(jumpDirection, jumpValue * jumpSpeed);
         jumpValue = 0.0f;
         jumpDirection = 0.0f;
         isGrounded = false;
+        SimpleEventManager.TriggerEvent("JumpEvent", new Dictionary<string, object>
+        {
+            {"jumper", this}
+        });
     }
 
-    void setAnim()
+    private void SetAnim()
     {
         anim.SetBool("isRunning", jumpValue == 0 && moveInput != 0 && isGrounded);
         anim.SetBool("isCharge", jumpValue > 0 && isGrounded);
         anim.SetBool("isJumping", !isGrounded && rb.velocity.y > 0);
         anim.SetBool("isFalling", !isGrounded && rb.velocity.y < 0);
     }
-
 }
-
--- Assets/Scripts/PowerUp/IncreaseWalk.cs
+++ Assets/Scripts/PowerUp/IncreaseWalk.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class IncreaseWalk : BasePowerUp
 {
     public float walkIncrease = 0.5f;
-    
+
     protected override void HandleInteract(GameObject collidedGameObject)
     {
         var jumper = collidedGameObject.GetComponent<Jumper>();
